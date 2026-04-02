e2f2d49ed7bd1abd80e7857cf473a5e143f1cbae
JC <576461713@qq.com>
2026-04-02 14:45:15 +0800

    fix(table): normalize column width values and restore wrap fit-content

--- src/main.js
+++ src/main.js
@@ -1,6 +1,4 @@
 import { createApp } from 'vue'
-import ElementPlus from 'element-plus'
-import 'element-plus/dist/index.css'
 import App from './App.vue'
 import './style.css'
 import './a2ui-runtime/style/global.css'
@@ -12,6 +10,5 @@ const app = createApp(App)
 sweetBase.i18n('zh_CN', app);
 sweetBase.setTheme('dark');
 app.use(sweetBase);
-app.use(ElementPlus)
 app.directive('safe-html', vSafeHtml)
 app.mount('#app')
