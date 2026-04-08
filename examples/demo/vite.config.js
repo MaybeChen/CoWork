152e430bbe76e4a4a640b68fb32302815ad139b7
JC <576461713@qq.com>
2026-04-08 09:44:05 +0800

    chore(vite): configure demo dev proxy and set playground port 3001

---
+++ examples/demo/vite.config.js
@@ -0,0 +1,35 @@
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import { fileURLToPath, URL } from 'node:url'
+const serverAdds = {
+  dev: 'http://10.136.125.119:8010',
+  prod: 'https://astr-lab.gts.huawei.com/',
+}
+
+const devServer = serverAdds.dev
+
+export default defineConfig({
+  plugins: [vue()],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
+  },
+  server: {
+    https: false,
+    host: '10.136.122.46',
+    port: 3000,
+    cors: true,
+    open: true,
+    proxy: {
+      '/api/chat/stream': {
+        target: devServer,
+        changeOrigin: true,
+        secure: false,
+        headers: {
+          Referer: devServer,
+        },
+      },
+    },
+  },
+})
