b9bfe7fa545e01a0f192efc62d75dd69fab67678
JC <576461713@qq.com>
2026-04-21 16:06:07 +0800

    refactor(demo): extract node builtin inputs into dedicated module

---
+++ examples/demo/src/modules/flow/nodeInputs.js
@@ -0,0 +1,21 @@
+export const NODE_INPUTS = {
+  log: `{
+  "result": {
+    "logResult": [],
+    "ciInfo": {
+      "ciName": "模型服务千问实例",
+      "ciId": "14537ad8-df06-4fb9-a584-e3a30b80b877",
+      "ciType": "InferenceModelInstance",
+      "ciSubType": "InferenceModelInstance"
+    }
+  }
+}`,
+  alert: '222',
+  ticket: '333',
+  metric: '444',
+  topology: '555',
+  knowledge: '666',
+  command: '777',
+  judge: '999',
+  inference: '结果123456',
+}
