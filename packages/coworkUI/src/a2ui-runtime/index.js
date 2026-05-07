e46bf62b057bdb6974170d507b96abbc4777512b
JC <576461713@qq.com>
2026-05-07 10:34:49 +0800

    fix(coworkUI): theme-adapt topology edge label colors for dark mode

--- packages/coworkUI/src/a2ui-runtime/index.js
+++ packages/coworkUI/src/a2ui-runtime/index.js
@@ -3,6 +3,3 @@ export { defaultRegistry } from './defaultRegistry'
 export { default as A2UIComponentRenderer } from './A2UIComponentRenderer.vue'
 export { defaultTheme } from './theme'
 
-import { injectA2UIStyles } from './style/styleManager'
-
-injectA2UIStyles()
